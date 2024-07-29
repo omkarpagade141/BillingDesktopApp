8f620004f4ce736c0c344a943655e541577d0a62
Omkar Pagade <omkarpagade141@gmail.com>
2024-07-30 00:41:18 +0530

    old backend deleted

--- backend/Project2_Test1/src/main/java/com/alpha/app/Exception/ResourceNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.alpha.app.Exception;
-
-public class ResourceNotFoundException extends RuntimeException{
-
-	public ResourceNotFoundException(String msg) {
-		// TODO Auto-generated constructor stub
-		super(msg);
-	}
-}
