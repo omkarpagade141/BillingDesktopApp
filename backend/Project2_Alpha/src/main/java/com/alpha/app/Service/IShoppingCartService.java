e5c1a14b04f1fa878aca3a0a99a4f1b9ffaf7ee9
NikhilD-1007 <nikhildhage1@gmail.com>
2024-08-12 11:01:09 +0530

    cart product created. cart GET, POST , DELETE working

--- backend/Project2_Alpha/src/main/java/com/alpha/app/Service/IShoppingCartService.java
+++ backend/Project2_Alpha/src/main/java/com/alpha/app/Service/IShoppingCartService.java
@@ -1,9 +1,19 @@
 package com.alpha.app.Service;
 
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+
+import com.alpha.app.DTO.RemoveProductFromCartDTO;
+import com.alpha.app.Entity.CartProducts;
 import com.alpha.app.Exception.UserHandlingException;
 
 public interface IShoppingCartService {
 
 	void addProductToCart(Long custId, Long prodId, int quantity) throws UserHandlingException;
+
+	List<CartProducts> getCartProductList(long custId);
+
+	void removeSingleProduct(RemoveProductFromCartDTO removeProd);
 
 }
